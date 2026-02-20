75cc819b7e9c39f572023656dcbb3c8aff3188b9
MD Ratul <tahid7266@gmail.com>
2026-02-20 16:30:44 +0600

    small bug fix

--- script/home.js
+++ script/home.js
@@ -1,20 +1,20 @@
 console.log('yes,iam home section')
 
 document.getElementById('add-money').addEventListener('click',function(){
-    window.location.assign("/add-money.html")
+    window.location.assign("add-money.html")
 })
 document.getElementById('cashout').addEventListener('click',function(){
-    window.location.assign("/cashout.html")
+    window.location.assign("cashout.html")
 })
 document.getElementById('send-money').addEventListener('click',function(){
-    window.location.assign("/send.html")
+    window.location.assign("send.html")
 })
 document.getElementById('get-bonus').addEventListener('click',function(){
-    window.location.assign("/bonus.html")
+    window.location.assign("bonus.html")
 })
 document.getElementById('pay-bill').addEventListener('click',function(){
-    window.location.assign("/pay-bill.html")
+    window.location.assign("pay-bill.html")
 })
 document.getElementById('Transactions').addEventListener('click',function(){
-    window.location.assign("/transactions.html")
+    window.location.assign("transactions.html")
 })
