02ded9636a137542d298396867edb4bfe204bc02
MD Ratul <tahid7266@gmail.com>
2026-02-21 02:28:24 +0600

    Add money section

--- script/addMoney.js
+++ script/addMoney.js
@@ -1,3 +1,26 @@
-document.getElementById('add-money-btn').addEventListener('click',function(){
-    
-})
+document.getElementById("add-money-btn").addEventListener("click", function () {
+  const bankAccount = getValueFromInput("add-money-bank");
+  if (bankAccount == "Select a Bank") {
+    alert("Please select a bank");
+    return;
+  }
+
+  const accno = getValueFromInput("add-money-number");
+  if (accno.length != 11) {
+    alert("Invalid Account Number");
+    return;
+  }
+
+  const amount = getValueFromInput("add-money-amount");
+  const currentBalance = getBalance();
+  const newBalance = currentBalance + Number(amount);
+
+  const pin = getValueFromInput("add-money-pin");
+  if (pin === "7266") {
+    alert(`Add Money Success from ${bankAccount} at ${new Date()}`);
+    setBalance(newBalance);
+  } else {
+    alert("Invalid Pin");
+    return;
+  }
+});
